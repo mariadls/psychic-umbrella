62d31f403480c4157a492cced0fc25567ca03773
mdoily <mariaydls@outlook.com>
2021-12-06 19:41:14 -0500

    added console.log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,35 @@
-window.alert("This is an alert! JavaScript is running!");
+//window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("Whats is your robot's name?");
+// Note the lack of quoatation marks around playerName
+//window.alert(playerName) 
+ // replace window.alert(playerName) with this:
+ //what is this?
+ console.log(playerName);
+
+ console.log("This logs a string, good for leaving yourself a message");
+ //this will do math and log 20
+ console.log(10 + 10);
+ //what is this?
+ console.log("Our robot's name is " + playerName);
+
+// this creates a function named "fight"
+function fight () {
+    window.alert("The fight has begun!");
+}
+
+//fight();
+
+// This is a String data type; it must be wrapped in double quotes (" ") or single quotes (' ').
+// var stringDataType = "this is a string, which is a fancy way to say text";
+
+// This is a Number data type; it can be an integer (whole number) or have decimals (floated numbers). 
+// var numberIntergerDataType= 10;
+// var numberFloatDataType = 10.4;
+
+// This is a Boolean data type; which can only be given a value of true or false.
+// var booleanDataType =  true;
+
+
+// This weeks assigment: Password generator. will need to take user imput and use conditional statements to determine what is going to be included in the password and how long it is going to be. 
+
+// In the class reposotry, there are now, each week, three algoritm projects, which we can use to pratice. HIGHLY reccommend we do them. Good to get ready. 
